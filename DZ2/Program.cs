72ddfbfaa3d5bf2d42d0d2e8638bded166a3429c
Mihail <Dante9041@mail.ru>
2022-06-30 15:02:22 +0300

    add DZ2

--- DZ2/Program.cs
+++ DZ2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double k1= Input("k1= ");
+double b1= Input("b1= ");
+double k2= Input("k2= ");
+double b2= Input("b2= ");
+
+if (k1==k2)
+{
+    Console.WriteLine("Линии парралельные");
+    return;
+}
+
+double x = (b1-b2)/(k1-k2);
+double y = k1*x+b1;
+
+Console.WriteLine($"пересекаються в координатах (x={x},y={y})");
+
+int Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
